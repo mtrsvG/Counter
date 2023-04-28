fd1dc5daf6990eb70f07b30e3777fb932f84cd7a
mtrsvG <mkrnv.gshn@gmail.com>
2023-04-28 11:55:55 +0300

    Made all interface and actions according to the technical assignment

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -7,13 +7,84 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+var count = 0
+var text = ""
 
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var historyTextView: UITextView!
+    @IBOutlet weak var counterLabel: UILabel!
+    @IBOutlet weak var increaseButton: UIButton!
+    
+    @IBOutlet weak var resetButton: UIButton!
+    @IBOutlet weak var decreaseButton: UIButton!
+    
+    @IBAction func decreseCount(_ sender: Any) {
+        decreseTheCount()
+    }
+    @IBAction func increaseCount(_ sender: Any) {
+        increaseTheCount()
+    }
+    @IBAction func resetCount(_ sender: Any) {
+        resetTheCount()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUpStartingView()
         // Do any additional setup after loading the view.
     }
+    
+    func setUpLabel (){
+        counterLabel.text = String(count)
+    }
 
-
+    func increaseTheCount(){
+        count += 1
+        setUpLabel()
+        text = getCurrentDateTime() + ": значение изменено на +1 \n"
+        addInfoAboutAction(text)
+    }
+    
+    func decreseTheCount() {
+        if count == 0 {
+            text = getCurrentDateTime() + ": попытка уменьшить значение счётчика ниже 0 \n"
+            addInfoAboutAction(text)
+        } else{
+            count -= 1
+            setUpLabel()
+            text = getCurrentDateTime() + ": значение изменено на -1 \n"
+            addInfoAboutAction(text)
+        }
+    }
+    
+    func resetTheCount(){
+        count = 0
+        setUpLabel()
+        text = getCurrentDateTime() + ": значение сброшено \n"
+        addInfoAboutAction(text)
+    }
+    
+    func getCurrentDateTime() -> String{
+        let date = Date()
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm"
+        return "\(dateFormatter.string(from: date))"
+        
+    }
+    // Функция вывода информации в текстовое поле с ее автопрокруткой
+    func addInfoAboutAction(_ text: String){
+        historyTextView.text += text
+        let range = NSRange(location: historyTextView.text.count - 1, length: 0)
+        historyTextView.scrollRangeToVisible(range)
+    }
+}
+extension ViewController{
+    func setUpStartingView() {
+        counterLabel.text = String(count)
+        historyTextView.isEditable = false
+        historyTextView.font = UIFont(name: "System", size: 10)
+        historyTextView.text = "История изменений: \n"
+    }
 }
 
